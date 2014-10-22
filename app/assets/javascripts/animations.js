e8bd44d00adcfc1dc657ff34034849d77791d679
Cynthia Sanchez <csanchez@suse.com>
2014-10-22 11:59:53 +0200

    homepage with initial Parallax

--- app/assets/javascripts/animations.js
+++ app/assets/javascripts/animations.js
@@ -1,8 +1,13 @@
 // functions for animations in general
 $(document).on("ready", function() {
   var parallaxSettings = { 
-    initialOpacity: 1, //from 0 to 1, e.g. 0.34 is a valid value. 0 = transparent, 1 = Opaque
-    opacitySpeed: 0.1 //values from 0.01 to 1 -> 0.01: slowly appears on screen; 1: appears as soon as the user scrolls 1px
+    initialOpacity: 1,
+    opacitySpeed: 1
   };
   parallaxImgScroll(parallaxSettings);
+  
+  // for test mode only
+  $("#hideTestArea").on("click", function(){
+    $(".testArea").fadeOut();
+  });
 });
